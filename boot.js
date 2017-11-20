654c6cbcdd8f10fadf526b4f0b778421190d3343
Péterfia Tamás <peterfiathomas@gmail.com>
2017-11-20 15:23:54 +0100

    névjegy

--- boot.js
+++ boot.js
@@ -1,17 +1,30 @@
-$(document).ready(function(){
+$(document).ready(function () {
     $('.but').on('click', le);
+    $('.box').mouseenter(szin);
+    $('.box').mouseleave(szinle);
 });
 
 
 function le()
 {
-    if($(this).parent().find('span').is(':hidden'))
+    if ($(this).parent().find('span').is(':hidden'))
     {
         $(this).parent().find('span').slideDown();
         $(this).parent().find('.but').html('Kevesebb &laquo;');
-    }else
+    } else
     {
         $(this).parent().find('span').slideUp();
         $(this).parent().find('.but').html('Több &raquo;');
     }
+}
+
+function szin()
+{
+    $(this).css({"background-color": "#B6C4CC"});
+    $(this).css({"box-shadow": "5px 5px 5px black"});
+}
+function szinle()
+{
+    $(this).css({"background": ""});
+    $(this).css({"box-shadow": ""});
 }
